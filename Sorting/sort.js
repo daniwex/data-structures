a273ddd771967cd66e458b85b94874f9a25d145f
adenikinju daniel <adenikinjudaniel@gmail.com>
2024-06-05 23:34:18 -0400

    new algorithms

--- Sorting/sort.js
+++ Sorting/sort.js
@@ -1,25 +1,91 @@
+const bubble = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        let temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+      }
+    }
+  }
+  return arr;
+};
 
-const bubble = function(arr){
-    for(let i=0;i<arr.length;i++){
-        for(let j=i+1;j<arr.length;j++){
-            if(arr[i] > arr[j]){
-                let temp = arr[j];
-                arr[j] = arr[i];
-                arr[i] = temp
-            }
+const selection = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    let temp = arr[i];
+    arr[i] = arr[min];
+    arr[min] = temp;
+  }
+  return arr;
+};
+
+const Insertion = function (arr) {};
+
+function merge(left, right) {
+  let sortedArr = []; // the sorted items will go here
+  while (left.length && right.length) {
+    // Insert the smallest item into sortedArr
+    if (left[0] < right[0]) {
+      sortedArr.push(left.shift());
+    } else {
+      sortedArr.push(right.shift());
+    }
+  }
+  // Use spread operators to create a new array, combining the three arrays
+  return [...sortedArr, ...left, ...right];
+}
+function partition(arr, low, high) {
+    // Choosing the pivot
+    let pivot = arr[high];
+  
+    // Index of smaller element and indicates the right position of pivot found so far
+    let i = low - 1;
+  
+    for (let j = low; j <= high - 1; j++) {
+        // If current element is smaller than the pivot
+        if (arr[j] < pivot) {
+            // Increment index of smaller element
+            i++;
+            [arr[i], arr[j]] = [arr[j], arr[i]]; // Swap elements
         }
     }
-return arr
+  
+    [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]]; // Swap pivot to its correct position
+    return i + 1; // Return the partition index
 }
 
-const selection = function(arr){
-    for(let i = 0;i<arr.length;i++){
-        for(let j=i;j<arr.length;j++){
-            
-        }
+
+const quickSort = (arr, low, high) => {
+    if (low < high) {
+        // pi is the partitioning index, arr[pi] is now at the right place
+        let pi = partition(arr, low, high);
+  
+        // Separately sort elements before partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
     }
-    return arr
 }
 
+const mergeSort = function (arr) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  // Recursive calls
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+};
 
-module.exports = {bubble,selection} 
+const selectionStr = function (arr) {
+  let m = arr.split("");
+  bubble(m);
+  return m.join("");
+};
+
+module.exports = { bubble, selection, selectionStr, Insertion, mergeSort, quickSort };
--- Sorting/sortA.js
+++ Sorting/sortA.js
@@ -3,4 +3,8 @@ const sorting = require('./sort')
 
 
 let arr = [2,1,4,8,3,5,10,0,0,12,9]
-console.log(sorting.selection(arr))
+let str = 'ayhbnjkpo'
+// m = str.slice(1)
+// console.log(m)
+// console.log(sorting.selectionStr(str))
+console.log(sorting.quickSort(arr, 0, arr.length - 1))
