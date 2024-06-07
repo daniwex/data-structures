d73385b8c417e528d58eb3bafa75e464f66e70c4
daniwex <adenikinjudaniel@gmail.com>
2024-06-07 09:07:52 -0400

    added new algorithms

--- Sorting/sort.js
+++ Sorting/sort.js
@@ -41,37 +41,27 @@ function merge(left, right) {
   // Use spread operators to create a new array, combining the three arrays
   return [...sortedArr, ...left, ...right];
 }
-function partition(arr, low, high) {
-    // Choosing the pivot
-    let pivot = arr[high];
-  
-    // Index of smaller element and indicates the right position of pivot found so far
-    let i = low - 1;
-  
-    for (let j = low; j <= high - 1; j++) {
-        // If current element is smaller than the pivot
-        if (arr[j] < pivot) {
-            // Increment index of smaller element
-            i++;
-            [arr[i], arr[j]] = [arr[j], arr[i]]; // Swap elements
-        }
-    }
-  
-    [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]]; // Swap pivot to its correct position
-    return i + 1; // Return the partition index
-}
 
 
-const quickSort = (arr, low, high) => {
-    if (low < high) {
-        // pi is the partitioning index, arr[pi] is now at the right place
-        let pi = partition(arr, low, high);
-  
-        // Separately sort elements before partition and after partition
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+const quickSort = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  let pivot = arr[0];
+  let leftArr = [];
+  let rightArr = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      leftArr.push(arr[i]);
+    } else {
+      rightArr.push(arr[i]);
     }
-}
+  }
+
+  return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
+};
 
 const mergeSort = function (arr) {
   if (arr.length <= 1) return arr;
@@ -88,4 +78,21 @@ const selectionStr = function (arr) {
   return m.join("");
 };
 
-module.exports = { bubble, selection, selectionStr, Insertion, mergeSort, quickSort };
+let sortString = (str) => {
+  return str.split('').reduce((sortedString, char) => {
+      let inserted = false;
+      for (let i = 0; i < sortedString.length; i++) {
+          if (char < sortedString[i]) {
+              sortedString = sortedString.slice(0, i) + char + sortedString.slice(i);
+              inserted = true;
+              break;
+          }
+      }
+      if (!inserted) {
+          sortedString += char;
+      }
+      return sortedString;
+  }, '');
+}
+
+module.exports = { bubble, selection, selectionStr, Insertion, mergeSort, quickSort, sortString };
--- Sorting/sortA.js
+++ Sorting/sortA.js
@@ -6,5 +6,5 @@ let arr = [2,1,4,8,3,5,10,0,0,12,9]
 let str = 'ayhbnjkpo'
 // m = str.slice(1)
 // console.log(m)
-// console.log(sorting.selectionStr(str))
-console.log(sorting.quickSort(arr, 0, arr.length - 1))
+console.log(sorting.selectionStr(str))
+// console.log(sorting.quickSort(arr, 0, arr.length - 1))
